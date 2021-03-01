ef044c3cde3dac754839480aa23fca00c7817a9d
unknown <diogo.vala@ua.pt>
2021-03-01 20:18:23 +0000

    Polling a funcionar

--- main/statemachine.c
+++ main/statemachine.c
@@ -1,21 +1,21 @@
-/* 
- * File:   statemachine.c
- * Author: Diogo Vala & Diogo Fernandes
- *
- * Overview: Define state machine 
- */
+/*
+   File:   statemachine.c
+   Author: Diogo Vala & Diogo Fernandes
+
+   Overview: Define state machine
+*/
 
 #include "statemachine.h"
 
 void sm_init(sm_t *psm, sm_state_t initial_state)
 {
-  psm->current_state=initial_state;
-  psm->initial_state=initial_state;
-  psm->last_event=ev_NULL;
+  psm->current_state = initial_state;
+  psm->initial_state = initial_state;
+  psm->last_event = ev_NULL;
 }
 void sm_send_event(sm_t *psm, sm_event_t event)
 {
-  psm->last_event=event;
+  psm->last_event = event;
 }
 sm_state_t sm_get_current_state(sm_t *psm)
 {
--- main/statemachine.h
+++ main/statemachine.h
@@ -1,15 +1,15 @@
-/* 
- * File:   statemachine.h
- * Author: Diogo Vala & Diogo Fernandes
- *
- * Overview: Define state machine 
- */
+/*
+   File:   statemachine.h
+   Author: Diogo Vala & Diogo Fernandes
+
+   Overview: Define state machine
+*/
 
 #ifndef STATEMACHINE_H
 #define STATEMACHINE_H
 
 //State machine states
-typedef enum{
+typedef enum {
   st_OFF,
   st_ON,
   st_CYCLESTART,
@@ -17,12 +17,12 @@ typedef enum{
   st_RAISETEMP,
   st_SEAL,
   st_ALARM
-}sm_state_t;
+} sm_state_t;
 
 #define sm_state_t int
 
 // State machine events
-typedef enum{
+typedef enum {
   ev_NULL,
   ev_ENABLE_HIGH,
   ev_ENABLE_LOW,
@@ -35,7 +35,7 @@ typedef enum{
   ev_TEMPSET,
   ev_RESET,
   ev_ALARM
-}sm_event_t;
+} sm_event_t;
 
 //State machine definition
 typedef struct sm_t
@@ -43,70 +43,70 @@ typedef struct sm_t
   sm_state_t current_state;
   sm_state_t initial_state;
   sm_event_t last_event;
-}sm_t;
+} sm_t;
 
 //extern sm_t SM;
 
 /********************************************************************
- * Function:    sm_init()
- * Precondition: Define sm_state_t and sm_event_t structs
- * Input:      *psm (Pointer to sm_t) ; initial_state
- * Returns:      
- *               
- * Side Effects: 
- *               
- * Overview:     Initiates the state machine at initial_state
- *    
- * Note:     last_event is set to ev_NULL
- * 
+   Function:    sm_init()
+   Precondition: Define sm_state_t and sm_event_t structs
+   Input:      *psm (Pointer to sm_t) ; initial_state
+   Returns:
+
+   Side Effects:
+
+   Overview:     Initiates the state machine at initial_state
+
+   Note:     last_event is set to ev_NULL
+
  ********************************************************************/
 void sm_init(sm_t *psm, sm_state_t initial_state);
 
 /********************************************************************
- * Function:    sm_send_event()
- * Precondition: states and events should be defined; 
- *               state machine should be initiated
- * Input:      *psm (Pointer to sm_t) ; event that occurred
- * Returns:      
- *               
- * Side Effects: 
- *               
- * Overview:     Sets last_event of sm_t to event
- *    
- * Note:     
- * 
+   Function:    sm_send_event()
+   Precondition: states and events should be defined;
+                 state machine should be initiated
+   Input:      *psm (Pointer to sm_t) ; event that occurred
+   Returns:
+
+   Side Effects:
+
+   Overview:     Sets last_event of sm_t to event
+
+   Note:
+
  ********************************************************************/
 void sm_send_event(sm_t *psm, sm_event_t event);
 
 /********************************************************************
- * Function:    sm_get_current_state()
- * Precondition: 
- *               
- * Input:      *psm (Pointer to sm_t)
- * Returns:    current_state of sm_t
- *               
- * Side Effects: 
- *               
- * Overview:    
- *    
- * Note:     
- * 
+   Function:    sm_get_current_state()
+   Precondition:
+
+   Input:      *psm (Pointer to sm_t)
+   Returns:    current_state of sm_t
+
+   Side Effects:
+
+   Overview:
+
+   Note:
+
  ********************************************************************/
 sm_state_t sm_get_current_state(sm_t *psm);
 
 /********************************************************************
- * Function:    sm_execute()
- * Precondition: 
- *               
- * Input:      *psm (Pointer to sm_t)
- * Returns:    
- *               
- * Side Effects: 
- *               
- * Overview:   Manages the state machine
- *    
- * Note:     Should be manually added in main file
- * 
+   Function:    sm_execute()
+   Precondition:
+
+   Input:      *psm (Pointer to sm_t)
+   Returns:
+
+   Side Effects:
+
+   Overview:   Manages the state machine
+
+   Note:     Should be manually added in main file
+
  ********************************************************************/
 void sm_execute(sm_t *psm);
 
